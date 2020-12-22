26d0f041b2822ec28b3fc73803ad121d38855959
traver9044@gmail.com <traver9044@gmail.com>
2020-12-22 17:13:10 +0900

    2020/12/22 수업예제

---
+++ Ch03/3_1_Condition.py
@@ -0,0 +1,50 @@
+"""
+날짜 : 2020/12/22
+이름 : 박준우
+내용 : 파이썬 조건문 실습하기
+"""
+
+# if
+num1, num2 = 1, 2
+if num1 > 0:
+    print('num1은 0보다 크다.')
+if num1 < num2:
+    print('num1은 num2보다 작다.')
+if num1 > 0:
+    if num2 > 1:
+        print('num1은 0보다 크고 num2는 1보다 크다.')
+if num1 > 0 and num2 > 1:
+    print('num1은 0보다 크고 그리고 num2는 1보다 크다.')
+
+# if-else
+num3, num4 = 3, 4
+if num3 > num4:
+    """조건이 참일때"""
+    print('num3은 num4 보다 크다.')
+else:
+    """조건이 거짓일때"""
+    print('num3은 num4 보다 작다.')
+
+# if ~elif ~else
+if num1 > num2:
+    print('num1은 num2보다 크다.')
+elif num2 > num3:
+    print('num2은 num3보다 크다.')
+elif num3 > num4:
+    print('num3은 num4보다 크다.')
+else:
+    print('num4가 가장 크다.')
+
+# 연습문제
+score = 86
+
+if score >= 90 and score <= 100:
+    print('A입니다.')
+elif score >= 80 and score < 90:
+    print('B입니다.')
+elif score >= 70 and score < 80:
+    print('C입니다.')
+elif score >= 60 and score < 70:
+    print('D입니다.')
+else:
+    print('F입니다.')
